1ade3231ac22efb518fb5a5207f7c0b97d4e201d
nzhang7 <25987490+nzhang7@users.noreply.github.com>
2019-03-01 03:58:24 -0800

    Add files via upload
    populated arrays with shop data

---
+++ optimal_armor_set_calc.js
@@ -0,0 +1,19 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var helms = [["Serpentine Cruz Headpiece",90,23],["Keeton Mask",77,24],["Feline Visor",68,16],["Ornate Helmet of Cagampan",60,16],
+             ["Offner Protector",54,15],["Leather Helmet",49,13],["Sligar's Noggin Protector",46,12],["Glass Bowl",44,12]];
+var chests = [["Armor de Jandro",67,22],["Chestpiece of Vachon",64,23],["Kaer Morhen Armor",62,21],["Cured Leather Chestpiece",59,20],
+              ["Smith's Plated Chestguard",58,10],["Dented Plate Armor",57,19],["Jeweled Drake Tunic",55,19],["Ginger's Gilded Armor",54,18],
+              ["Garcia Guard",50,17]];
+var legs = [["Famed Pon Leggings",87,22],["Ursine Trousers",78,18],["Wolven Shinguards",75,15],["Hansen's Breeches",69,17],
+            ["Griffin Pants",62,11],["Tanned Leg Protection",59,15],["Manticore Braces",56,12],["Mail Emares Leggings",53,14],
+            ["Woven Leggings",47,11],["Silken Pants",45,10],["Tattered Shorts",42,13]];
+var boots = [["Diamond Boots",64,18],["Steel Boots",52,14],["Tate's Spiked Cleats",52,20],["Leather Lunde Shoes",35,7],
+             ["Cloth Shoes",33,5]];
