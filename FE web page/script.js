7f8c8efc61485f6c4cd47926d89429083025d903
nzhang7 <25987490+nzhang7@users.noreply.github.com>
2019-03-17 12:56:05 -0700

    Update script.js
    connected to all content pages of the API and added bare code to alter the HTML page

--- FE web page/script.js
+++ FE web page/script.js
@@ -1,14 +1,32 @@
-// create a request variable and assign an xmlhttprequest object to it
-var request = new XMLHttpRequest();
-// open a connection to ign's api with a get request (cors proxy used)
-xhr.open('GET', 'http://localhost:8080/https://ign-apis.herokuapp.com/content', true);
+// an array of request variables we will use to assign xmlhttprequests to soon
+var request = [];
 
-xhr.onload = function(){
-	const app = document.getElementById('root');
+// i want to gather information from all entries of the api, startindex goes up to 300 and 20 can be displayed at once
+// 300/20 = 15, our upper limit on the for loop
+for (let i = 0; i <= 15; i++){
+	// assign an XHR object to one of the request variables
+	request[i] = new XMLHttpRequest();
+	// open a connection to ign's api with a get request (cors proxy used), start index of url increments by 20 since we display 20 at once
+	request[i].open('GET','http://localhost:8080/https://ign-apis.herokuapp.com/content?startIndex=' + i*20 + '&count=20',true);
 	
-	var data = JSON.parse(this.response);
-	console.log(data.data[0]);
-	
-};
-// send request
-xhr.send();
+	request[i].onload = function(){
+		// here, we will look at and do things with the data we've pulled
+		for (let j = 0; j < 20; j++){
+			// this will give us an object we can work with that contains the entire page of data, 
+			var data = JSON.parse(this.response);
+			
+			// these are for me okay don't look
+			console.log(data);
+			console.log(data.data[j].contentType);
+			
+			// articles in the api have headlines while videos have titles, and so we must differentiate them accordingly
+			// if it's an article, it's an article. if it's not, it's a movie. we are also adding those things to our HTML
+			if (data.data[j].contentType == "article")
+				document.getElementById("data" + data.startIndex/20).innerHTML += data.data[j].metadata.headline + "<br>";
+			else
+				document.getElementById("data" + data.startIndex/20).innerHTML += data.data[j].metadata.title + "<br>";
+		}
+	}
+	// send request
+	request[i].send();
+}
